e8675db4e424129bc7391cdf3125e8c3a3f43cb3
linzhu <lin.lyn.zhu@gmail.com>
2022-03-16 12:48:45 -0700

    write name

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function() {
   conn.setEncoding("utf8");
   conn.on('connect', () => {
     console.log('connected, game start');
+    conn.write('Name: Lyn');
   });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
